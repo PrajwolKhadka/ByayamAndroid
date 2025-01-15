5d9e304d706c53daaa9103428728b174faf71a6a
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-01-15 09:15:42 +0545

    push

--- app/src/main/java/com/example/byayamandroid/model/ProductModel.kt
+++ app/src/main/java/com/example/byayamandroid/model/ProductModel.kt
@@ -1,4 +1,10 @@
 package com.example.byayamandroid.model
 
-class ProductModel {
+data class ProductModel (
+    var product_id: String= "",
+    var product_name: String="",
+    var product_desc: String="",
+    var price : Int=0,
+){
+
 }
--- app/src/main/java/com/example/byayamandroid/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/byayamandroid/repository/ProductRepositoryImpl.kt
@@ -1,4 +1,36 @@
 package com.example.byayamandroid.repository
 
-class ProductRepositoryImpl {
+import com.example.byayamandroid.model.ProductModel
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class ProductRepositoryImpl: ProductRepository {
+
+    val database: FirebaseDatabase= FirebaseDatabase.getInstance()
+    val ref: DatabaseReference=database.reference.child("products")
+
+
+    override fun addProduct(productModel: ProductModel, callback: (Boolean, String) -> Unit) {
+        var id=ref.push().key.toString()
+
+    }
+
+    override fun updateProduct(productId: String, data: MutableMap<String, Any>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteProduct(productId: String, callback: (Boolean, String) -> Unit) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProductById(
+        productId: String,
+        callback: (ProductModel?, Boolean, String) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllProduct(callback: (List<ProductModel?>, Boolean, String) -> Unit) {
+        TODO("Not yet implemented")
+    }
 }
